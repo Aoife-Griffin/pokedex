975b3f7a46b0fa2b6352bbadab5ad2740313ff8d
Aoife Griffin <x00195309@mytudublin.ie>
2024-09-19 10:01:18 +0100

    Building base

--- Pokedex/main.py
+++ Pokedex/main.py
@@ -2,6 +2,18 @@ import pypokedex
 from PIL import image, imageTK
 import tkinter as tkinter
 import urllib3
-
-## Get image from link and handle bytes
+### Get image from link and handle bytes
 from io import BytesIO
+
+### Build baseline
+window = tk.Tk()
+window.geometry("500x500")
+window.title("Pokedex")
+window.config(padx=10, pady=10)
+
+title_label = tk.Label(window, text="Personal Pokedex")
+title_label.config(font=("Arial", 24))
+title_label.pack(padx=10, pady=10)
+
+
+window.mainloop()
