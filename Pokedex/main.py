cd8f5fc8cc5046fc79fd49c4ee43710ee48e7035
Aoife Griffin <x00195309@mytudublin.ie>
2024-09-19 10:42:05 +0100

    User Input & Fixing Errors in Code

--- Pokedex/main.py
+++ Pokedex/main.py
@@ -1,6 +1,6 @@
 import pypokedex
-from PIL import image, imageTK
-import tkinter as tkinter
+import PIL.Image, PIL.ImageTk
+import tkinter as tk
 import urllib3
 ### Get image from link and handle bytes
 from io import BytesIO
@@ -26,6 +26,20 @@ pokemon_information.pack(padx=10, pady=10)
 pokemon_types = tk.Label(window)
 pokemon_types.config(font=("Arial", 24))
 pokemon_types.pack(padx=10, pady=10)
+
+### Search for Pokemon
+label_id_name = tk.Label(window, text="Name or Number")
+label_id_name.config(font=("Arial", 20))
+label_id_name.pack(padx=10, pady=10)
+
+text_id_name = tk.Text(window, height=1)
+text_id_name.config(font=("Arial", 20))
+text_id_name.pack(padx=10, pady=10)
+
+button_load = tk.Label(window, text="Find Pokemon")
+button_load.config(font=("Arial", 20))
+button_load.pack(padx=10, pady=10)
+
 
 
 
