0aaabad5dda1fe745a7d7cec643ad38df48438db
Aoife Griffin <x00195309@mytudublin.ie>
2024-09-19 10:56:18 +0100

    Load Pokemon in

--- Pokedex/main.py
+++ Pokedex/main.py
@@ -27,6 +27,29 @@ pokemon_types = tk.Label(window)
 pokemon_types.config(font=("Arial", 24))
 pokemon_types.pack(padx=10, pady=10)
 
+### Function to load pokemon
+
+def load_pokemon():
+    # Get name or id that was passed in from starting to end poisition
+    pokemon = pypokedex.get(name=text_id_name.get(1.0, "end-1c"))
+    # Get sprite
+    http = urllib3.PoolManager()
+    response = http.request('GET', pokemon.sprites.front.get('default'))
+    
+    # Turn image into bytes and save to object
+    image = PIL.Image.open(BytesIO(response.data))
+    # Turn object into tkinter
+    img = PIL.ImageTk.PhotoImage(image)
+    pokemon_image.config(image=img)
+    pokemon_image.image = img
+
+    # Get information
+    pokemon_information.config(text=f"{pokemon.dex} - {pokemon.name}")
+
+    # Get type
+    pokemon_types.config(text=f"{pokemon.types}")
+
+
 ### Search for Pokemon
 label_id_name = tk.Label(window, text="Name or Number")
 label_id_name.config(font=("Arial", 20))
@@ -36,11 +59,9 @@ text_id_name = tk.Text(window, height=1)
 text_id_name.config(font=("Arial", 20))
 text_id_name.pack(padx=10, pady=10)
 
-button_load = tk.Label(window, text="Find Pokemon")
+button_load = tk.Button(window, text="Find Pokemon", command=load_pokemon)
 button_load.config(font=("Arial", 20))
 button_load.pack(padx=10, pady=10)
-
-
 
 
 
